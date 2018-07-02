018a66a46ae84699ef55d70cf5d67975587351a3
minutemedia <minutemedia@minutemedias-MacBook-Pro-2.local>
2018-07-02 14:01:07 +0300

    first commit

---
+++ JSONExer/JSONExer/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+
+namespace JSONExer
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            string path = @"/Users/minutemedia/Documents/JSONEx.txt";
+            Console.WriteLine("Insert a string");
+            string var = Console.ReadLine();
+            Console.WriteLine(var);
+            if (!File.Exists(path)) //Checking if file exists in a path
+            {
+                using (StreamWriter sw = File.CreateText(path))
+                {
+                    sw.Write(var);
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
